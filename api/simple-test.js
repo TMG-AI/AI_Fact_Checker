71322492e13492fe56bc687f6ae7bb6c82c67850
TMG-AI <swheatman@signalinteractive.com>
2025-06-23 00:27:13 -0400

    Update simple-test.js

--- api/simple-test.js
+++ api/simple-test.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
   if (req.method === 'OPTIONS') {
@@ -9,4 +9,4 @@ export default function handler(req, res) {
     message: "Simple test works!",
     method: req.method 
   });
-}
+};
